1017aaebd0924c4787e26d2737836a7b1ca4453e
jat-studio <jat-studio@yandex.ru>
2015-05-13 13:57:36 +0400

    added text drawing

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 int wnd;            // id GLwindow
 
-GLuint Texture[1];  // index of texture
+GLuint Font[1];     // index texture of font
+GLuint Tiles[1];    // index texture of tiles
 
 // parameters of image
 int width = 0;
@@ -20,6 +21,14 @@ GLfloat LightAmbient[] = {0.5, 0.5, 0.5, 1.0}; // array of ambient light
 GLfloat LightDiffuse[] = {1.0, 1.0, 1.0, 1.0}; // array of diffuse light
 GLfloat LightPosition[] = {0.5, 0.5, 0.5, 1.0}; // coordinates of light source
 
+// output string
+void DrawString(float x, float y, float z, void *font, char *string){
+  char *c;
+  glRasterPos3f(x, y,z);
+  for (c = string; *c != '\0'; c++){
+    glutBitmapCharacter(font, *c);
+  }
+}
 // loading texture by filename
 void LoadTexture(const char* texName){
   // initializing il and ilu library
@@ -66,43 +75,34 @@ void LoadTexture(const char* texName){
   }
 }
 
+// repainting OpenGL by reshape window
+void Reshape(GLsizei Width, GLsizei Height){
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glViewport(0, 0, Width, Height);
+  gluPerspective(45.0, (GLfloat)Width/(GLfloat)Height, 0.1, 100.0);
+  glMatrixMode(GL_MODELVIEW);
+}
+
 // painting Scene
 void Draw(void){
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity();
-  glTranslatef(0.0, 0.0, -6.0);
-  glBindTexture(GL_TEXTURE_2D, Texture[0]);
-    glBegin(GL_QUADS);
-      glNormal3f(0.0, 0.0, 1.0);
-      glTexCoord2f(0.0, 0.0); glVertex3f(-0.125, -0.125, 0.125);
-      glTexCoord2f(1.0, 0.0); glVertex3f(0.125, -0.125, -0.125);
-      glTexCoord2f(1.0, 1.0); glVertex3f(0.125, 0.125, 0.125);
-      glTexCoord2f(0.0, 1.0); glVertex3f(-0.125, 0.125, 0.125);
-    glEnd();
-  glutSwapBuffers();
-}
 
-// repainting OpenGL by reshape window
-void Reshape(GLsizei Width, GLsizei Height){
-  // fon color
-  glClearColor(0, 0, 0, 0);
-  // depth test
-  glClearDepth(GL_TRUE);
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LEQUAL);
-  // smoothing paint of color
-  glShadeModel(GL_SMOOTH);
-  // blending
-  glColor4f(1.0, 1.0, 1.0, 0.5); // full brightness 50% blending
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE);
-  // modificated perspective
-  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-  // window settings
-  glViewport(0, 0, Width, Height);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(45.0, (GLfloat)Width/(GLfloat)Height, 0.1, 100.0);
-  glMatrixMode(GL_MODELVIEW);
+  glPushMatrix();
+  glTranslatef(0.0, 0.0, -6);
+  glBindTexture(GL_TEXTURE_2D, Tiles[0]);
+  glBegin(GL_QUADS);
+    glNormal3f(0.0, 0.0, 1.0);
+    glTexCoord2f(0.0, 0.0); glVertex3f(-0.125, -0.125, 0.0);
+    glTexCoord2f(1.0, 0.0); glVertex3f(0.125, -0.125, 0.0);
+    glTexCoord2f(1.0, 1.0); glVertex3f(0.125, 0.125, 0.0);
+    glTexCoord2f(0.0, 1.0); glVertex3f(-0.125, 0.125, 0.0);
+  glEnd();
+  DrawString(0.0, 0.5, 0.0, GLUT_BITMAP_TIMES_ROMAN_24, "Hello!");
+  glPopMatrix();
+
+  glutSwapBuffers();
 }
 
 // processing keys
@@ -110,7 +110,7 @@ void Keyboard(unsigned char key, int x, int y){
   switch (key){
     // escape
     case 27:
-      glDeleteTextures(1, &Texture[0]);
+      glDeleteTextures(1, &Tiles[0]);
       glutDestroyWindow(wnd);
     case 'l':
       if (light){
@@ -140,17 +140,17 @@ void Keyboard(unsigned char key, int x, int y){
 int main(int argc, char *argv[]){
   // initializing and create window GLUT
   glutInit(&argc, argv);
-  glutInitWindowSize(1920, 1080);
-  glutInitWindowPosition(0, 0);
   glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-  wnd = glutCreateWindow("Open GL Hello World!");
+  glutInitWindowPosition(0, 0);
+  glutInitWindowSize(1920, 1080);
+  wnd = glutCreateWindow("Evolution 0.0.0");
 
   // getting data of textures of cube
   LoadTexture("grass.bmp");
   // loading textures into memory
-  glGenTextures(1, &Texture[0]);
+  glGenTextures(1, &Tiles[0]);
   // texture with linear filter
-  glBindTexture(GL_TEXTURE_2D, Texture[0]);
+  glBindTexture(GL_TEXTURE_2D, Tiles[0]);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   gluBuild2DMipmaps(GL_TEXTURE_2D, bpp, width, height, type, GL_UNSIGNED_BYTE, data_img);
@@ -171,9 +171,25 @@ int main(int argc, char *argv[]){
   glutReshapeFunc(Reshape);
   glutIdleFunc(Draw);
   glutKeyboardFunc(Keyboard);
+
+  // init GL
+  // fon color
+  glClearColor(0.5, 0.5, 0.5, 0);
+  // depth test
+  glClearDepth(GL_TRUE);
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LEQUAL);
+  // smoothing paint of color
+  glShadeModel(GL_SMOOTH);
+  // blending
+  glColor4f(1.0, 1.0, 1.0, 0.5); // full brightness 50% blending
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+  // modificated perspective
+  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
   // processing events of window
   glutMainLoop();
   // clear textures
-  glDeleteTextures(1, &Texture[0]);
+  glDeleteTextures(1, &Tiles[0]);
   return 0;
 }
